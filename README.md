9444ce05993b84c16d6e3dca060de20384440256
Matthew Hassell <hassell.matthew@gmail.com>
2017-04-30 08:04:11 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # gqrx_scan
+
+A scanner to interface with GQRX.  
+
+Simply loops over a list of frequencies in a csv and listens for activity.  
+
+TBD:
+
+	1. GUI
+
+	2. Ability to add frequencies in the GUI
+
+	3. Set squelch for each channel
+
+	4. Include tags for each channel
---
+++ gqrx_scan.py
@@ -0,0 +1,64 @@
+import telnetlib
+import csv
+import time
+
+class Scanner:
+
+	def __init__(self, hostname='127.0.0.1', port=7356, directory='/Users/matthewhassell/GitHub/misc', waitTime=5, signalStrength=-15):
+		self.host = hostname
+		self.port = port
+		self.directory = directory
+		self.waitTime = waitTime
+		self.signalStrength = signalStrength
+
+	def _update(self, msg):
+		"""
+		update the frequency/mode GQRX is listening to
+		"""
+		tn = telnetlib.Telnet(self.host, self.port)
+		tn.write(('%s\n' % msg).encode('ascii'))
+		response = tn.read_some().decode('ascii').strip()
+		tn.write('c\n'.encode('ascii'))
+		return response
+
+	def scan(self):
+		"""
+		loop over the frequencies in the list, and stop if the frequency is active (signal strength is high enough)
+		"""
+		while(1):
+			for freq in self.freqs.keys():
+				self._set_freq(freq)
+				self._set_mode(self.freqs[freq])
+				time.sleep(0.2)
+				if float(self._get_level()) >= self.signalStrength:
+					print freq
+					while float(self._get_level()) >= self.signalStrength:
+						time.sleep(self.waitTime)
+
+
+   	def load(self):
+   		"""
+   		read the csv file with the frequencies & modes in it into a dict{} where keys are the freq and values are the mode
+   		"""
+   		self.freqs = {}
+   		with open('freq.csv','r') as csvfile:
+   			reader = csv.reader(csvfile, delimiter = ',')
+   			for row in reader:
+   				freq = str(float(row[0])*1e5)		# 1e5 isn't good
+   				freq = int(freq.replace('.', '')) 	# converted to hz
+   				self.freqs[freq] = row[1]     		# add the freq to the dict as a key and the mode as the value
+
+
+   	def _set_freq(self, freq):
+   		return self._update("F %s" % freq)
+
+   	def _set_mode(self, mode):
+   		return self._update("M %s" % mode)
+
+   	def _get_level(self):
+   		return self._update("l")
+
+if __name__ == "__main__":
+	scanner = Scanner()
+	scanner.load()
+	scanner.scan()
